7c66020abe473ce4d551b5e8c9bd4ac3add4c0d7
udthegooner <udhbhav25@gmail.com>
2017-07-10 13:48:38 +0530

    Create lock.bat

---
+++ Password Manager/lock.bat
@@ -0,0 +1,25 @@
+REM batch file which unlocks and runs the password manager
+
+@ECHO OFF
+if EXIST "HiddenFolder" goto LOCK
+:UNLOCK
+echo Enter password to your Password Manager
+set/p "pass=>"
+
+REM default password is pw
+if NOT %pass%== pw goto FAIL
+attrib -h -s "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+ren "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}" HiddenFolder
+
+REM enter the path to your hidden folder
+python "HiddenFolder\pm.py"
+
+goto LOCK
+:FAIL
+echo Invalid password
+goto end
+:LOCK
+ren HiddenFolder "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+attrib +h +s "Control Panel.{21EC2020-3AEA-1069-A2DD-08002B30309D}"
+echo Password Manager locked
+:End
