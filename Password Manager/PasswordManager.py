0e0b3412591d9a5a7c47faa9237da94357ba241e
udthegooner <udhbhav25@gmail.com>
2017-07-10 13:49:02 +0530

    Add files via upload

---
+++ Password Manager/PasswordManager.py
@@ -0,0 +1,6 @@
+#script to run the hidden lock batch file which runs the password manager
+
+from subprocess import Popen
+#enter location of batch file
+p = Popen("lock.bat", cwd=r"")
+stdout, stderr = p.communicate()
