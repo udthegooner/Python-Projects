555fe4d25afbdbd70b50edaf67569b7a3033f3ed
udthegooner <udhbhav25@gmail.com>
2017-07-20 01:32:24 +0530

    Update lock.bat

--- Password Manager/lock.bat
+++ Password Manager/lock.bat
@@ -2,6 +2,7 @@
 REM batch file which unlocks and runs the password manager
 
 if EXIST "HiddenFolder" goto LOCK
+
 :UNLOCK
 echo Enter password to your Password Manager
 set/p "pass=>"
@@ -12,17 +13,18 @@ attrib -h -s "cbr12k3e"
 ren "cbr12k3e" "HiddenFolder"
 attrib -h "HiddenFolder\pws.txt"
 
-REM enter the absolute path to your hidden folder
-python "\HiddenFolder\pm.py"
+python "HiddenFolder\pm.py"
 
 goto LOCK
+
 :FAIL
 echo Invalid password
 goto end
+
 :LOCK
+attrib +h "HiddenFolder\pws.txt"
 ren "HiddenFolder" "cbr12k3e"
 attrib +h +s "cbr12k3e"
-attrib +h "HiddenFolder\pws.txt"
-
 echo Password Manager locked
+
 :End
